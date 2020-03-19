ed84d219a614af97516fc205a3f7cbcc22767b87
Kyle Greene <kylengreene@gmail.com>
2020-03-19 13:35:44 -0500

    500 error on post request for new alumni info

--- server/routes/alumni.router.js
+++ server/routes/alumni.router.js
@@ -9,15 +9,27 @@ const router = express.Router();
 
 
 router.post('/', (req, res, next) => {
-    console.log('logging from post inalumni router', req.body);
+    console.log('logging from post inalumni router', req.body.firstName);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const phoneNumber = req.body.phoneNumber;
+    const age = req.body.age;
+    const gender = req.body.gender;
+    const yearsAtCamp = req.body.yearsAtCamp;
+    const favoriteActivity = req.body.favoriteActivity;
+    const favoriteMemory = req.body.favoriteMemory;
+    const annualFund = req.body.annualFund;
+    const volunteerWork = req.body.volunteerWork;
+    const newsList = req.body.newsList;
+    const willingToBeContacted = req.body.willingToBeContacted;
+    console.log('logging after setting analogs:', lastName);
     
+    const queryText = 'INSERT INTO "user_info" ("firstName","lastName","email","phoneNumber","age","gender","yearsAtCamp","favoriteActivity","favoriteMemory","annualFund","volunteerWork","newsList","willingToBeContacted") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)';
 
-    // const queryText = 'INSERT INTO "user_info" (firstName,lastName,email,phoneNumber,age,gender,yearsAtCamp,favoriteActivity,favoriteMemory,annualFund,volunteerWork,newsList,willingToBeContacted) 
-    // VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) ';
-
-    // pool.query(queryText, [username, password])
-    //     .then(() => res.sendStatus(201))
-    //     .catch(() => res.sendStatus(500));
+    pool.query(queryText, [firstName, lastName, email, phoneNumber, age, gender, yearsAtCamp, favoriteActivity, favoriteMemory, annualFund, volunteerWork, newsList, willingToBeContacted ])
+        .then(() => res.sendStatus(201))
+        .catch(() => res.sendStatus(500));
 });
 
 
--- src/redux/sagas/aulmniRegistrationSaga.js
+++ src/redux/sagas/aulmniRegistrationSaga.js
@@ -7,22 +7,21 @@ function* alumniNewInfo(action) {
 
     yield console.log('logging from alumniNewInfo', action.payload);
     const objectToPost = action.payload
-    console.log( objectToPost);
-    
-        yield put({ type: 'SET_NEW_ALUMNI_INFO', data: action.payload })
-    // try {
-        
-    //     yield axios({
-    //         method: 'POST',
-    //         url: '/api/alumniInfo',
-    //         data: objectToPost 
-    //     })
-    //     // yield put({
-    //     //     type: 'GET_FRUIT'
-    //     // })
-    // } catch (error) {
-    //     console.log(error);
-    // }
+    console.log(objectToPost);
+
+    yield put({ type: 'SET_NEW_ALUMNI_INFO', data: action.payload })
+    try {
+        yield axios({
+            method: 'POST',
+            url: '/api/alumniInfo',
+            data: objectToPost
+        })
+        // yield put({
+        //     type: 'GET_FRUIT'
+        // })
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
