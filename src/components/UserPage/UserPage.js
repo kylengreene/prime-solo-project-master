a97d365ba5a949dbc3ba87e4d086fc1eb5bf372c
Kyle Greene <kylengreene@gmail.com>
2020-03-28 13:23:59 -0500

    image carousel working

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './UserPage.css';
-
-
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import Picture1 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_0098.png'
+import Picture2 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_0118.jpeg'
+import Picture3 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_0207.jpeg'
+import Picture4 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_7658.JPG'
+import Picture5 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_8056.JPG'
+import Picture6 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_8751.jpeg'
+import Picture7 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_9287.jpeg'
+import Picture8 from '/Users/kylegreene/Prime/Tier 3/Solo Project/prime-solo-project-master/src/pictures/IMG_9463.jpeg'
 // This is the page where the user will land upon log in. 
 //It includes a Facebook feed of camp updates as well as a search box to search for alumni. 
 
@@ -11,6 +19,7 @@ class UserPage extends Component {
     category: "firstName",
     search: ""
   };
+  
 
   searchAlumni = (event) => {
     event.preventDefault();
@@ -30,9 +39,30 @@ class UserPage extends Component {
   }
 
   render() {
+   
     return (
       <>
         <div className="UserPage">
+          <div className="carousel">
+          <Carousel
+              centered
+              slidesPerPage={3}
+              infinite
+              keepDirectionWhenDragging
+            autoPlay={3000}
+            animationSpeed={1000}
+            infinite
+          >
+            <img height='400' alt='camp icaghowan fun' src={Picture1}/>
+              <img height='400' src={Picture2} alt='camp icaghowan fun' />
+              <img height='400' src={Picture3} alt='camp icaghowan fun' />
+              <img height='400' src={Picture4} alt='camp icaghowan fun' />
+              <img height='400' src={Picture5} alt='camp icaghowan fun' />
+              <img height='400' src={Picture6} alt='camp icaghowan fun' />
+              <img height='400' src={Picture7} alt='camp icaghowan fun' />
+              <img height='400' src={Picture8} alt='camp icaghowan fun' />
+          </Carousel>
+        </div>
           <h1 id="welcome">
             Welcome, {this.props.user.username}!
     </h1>
